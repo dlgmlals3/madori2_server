64e94d956965ae5efae96b3a73b3d73aedf510f6
simon <dlgmlals3@gmail.com>
2018-07-09 23:37:32 +0900

    [madori2][server] add mongodb framework
    Change-Id: Ica4910a1ce4a2bf3cb34b6f2a2c838b66125b13a

--- app.js
+++ app.js
@@ -11,22 +11,25 @@ var app = express();
 var port = 3333;
 
 app.set('port', process.env.PORT || port);
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
+app.use(bodyParser.urlencoded ({ extended: false }));
 app.use(bodyParser.json());
-
-var router = require('./routes/routers')(app);
 
 var errorHandler = expressErrorHandler({
   static: {
     '404': './public/404.html'
   }
 });
-
 app.use(expressErrorHandler.httpError(404));
 app.use(errorHandler);
+
+var router = require('./routes/routers')(app);
+
+var MongoClient = require('mongodb').MongoClient;
+var dbclient = require('./database/db_api.js');
+var database = dbclient.connectDB(MongoClient);
+// db test api
+dbclient.findPassword(database, "dlgmlals3");
+
 var server = http.createServer(app).listen(app.get('port'), function() {
   console.log('express port : ' + app.get('port'));
 });
