f651944e97654848b00a28f06c5f333b3b129efe
simon <dlgmlals3@gmail.com>
2018-07-15 12:50:19 +0900

    [madori2][server] remove mongo db test api
    1. add parameter
    $ node app.js 3334
    
    2. add setheader code
    
    Change-Id: I1f90fcfc8ba21a1060af1b1405af7a294460bf23

--- app.js
+++ app.js
@@ -9,27 +9,37 @@ var expressSession = require('express-session');
 var expressErrorHandler = require('express-error-handler');
 
 var app = express();
-var port = 3333;
 
-app.set('port', process.env.PORT || port);
+var defaultPort = 3333;
+var port = process.argv[2];
+
+app.set('port',  port || defaultPort);
 app.use(bodyParser.urlencoded ({ extended: false }));
 app.use(bodyParser.json());
 
-var errorHandler = expressErrorHandler({
-  static: {
-    '404': './public/404.html'
-  }
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
+next();
 });
-app.use(expressErrorHandler.httpError(404));
-app.use(errorHandler);
 
 var router = require('./routes/routers')(app);
 
-var MongoClient = require('mongodb').MongoClient;
-var dbclient = require('./database/db_api.js');
-var database = dbclient.connectDB(MongoClient);
+//var errorHandler = expressErrorHandler({
+//  static: {
+//    '404': './public/404.html'
+//  }
+//});
+//app.use(expressErrorHandler.httpError(404));
+//app.use(errorHandler);
+
+//var MongoClient = require('mongodb').MongoClient;
+//var dbclient = require('./database/db_api.js');
+//var database = dbclient.connectDB(MongoClient);
 // db test api
-dbclient.findPassword(database, "dlgmlals3");
+//dbclient.findPassword(database, "dlgmlals3");
 
 var server = http.createServer(app).listen(app.get('port'), function() {
   console.log('express port : ' + app.get('port'));
