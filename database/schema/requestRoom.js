f0e11dcbcc6baa3c14e448519b056673b917b584
dlgmlals3 <dlgmlals3@gmail.com>
2019-01-05 17:46:35 +0900

    add apply room
    Change-Id: I53deedd8ab798b51d0955f43a8a654fc2a1c449f

---
+++ database/schema/requestRoom.js
@@ -0,0 +1,180 @@
+var mongoose = require('mongoose');
+var autoIncrement = require('mongoose-auto-increment');
+
+var requestRoomSchema = mongoose.Schema({
+		memberId : {type:String,required:true},
+		roomId:{type:mongoose.Schema.Types.ObjectId,ref:'room'},
+		requestStatus : {type:String,required:true}
+});
+requestRoomSchema.index({'memberId':1, 'roomId':1, 'requestStatus':1}, {unique: true});
+
+requestRoomSchema.statics.insertRequest = async function(req, roomObjId, status, res) {
+	requestObj = new this({
+		memberId:req.body.requestMemberId,
+		roomId:roomObjId,
+		requestStatus:status
+	});
+	var result = await requestObj.save(function (err, savedObj) {
+	  if (err) {
+			console.log('insertRequest error' + err);
+			res.json ({
+				statusCode: '500',
+				statusMsg: err,
+      	total: '0'
+			});
+		} else if (!savedObj) {
+			console.log('insertRequest error' + err);
+			res.json ({
+				statusCode: '500',
+				statusMsg: err,
+      	total: '0'
+			});
+		} else {
+			res.json ({
+				statusCode: '200',
+				statusMsg: 'success',
+      	total: '1'
+			});
+			console.log('Save obj : ' + savedObj);
+		}
+	})
+}
+
+requestRoomSchema.statics.getRequest = async function(roomObjId, res){
+	console.log("getRequest RoomId : " + roomObjId);
+  await this.find (
+		{"roomId": roomObjId, "requestStatus": "request"}, function(err, obj) {
+			if (err) {
+				console.log('getRequest err : ' + err);
+				res.json({
+      		statusCode: '500',
+      		statusMsg: err,
+      		total: '0'
+    		});
+			} else if (!obj) {
+				console.log('request not found');
+    		res.json({
+      		statusCode: '200',
+      		statusMsg: 'room not found',
+      		total: '0'
+    		});
+			} else {
+				console.log('request found success : ' + obj);
+					res.json({
+					  statusCode: '200',
+						statusMsg: 'success',
+						total: obj.length,
+						resultItems:obj
+				 });
+		 }
+ 	});
+}
+
+requestRoomSchema.statics.updateRequest = async function(req, res) {
+	console.log("updateRequest memberId : " + req.params.memberId);
+  await this.findOneAndUpdate(
+    {memberId : req.params.memberId},
+		{$set : 
+		  {
+				requestStatus : "approved"
+		  }
+		},
+	  function (err, obj) {
+			if (err) {
+				console.log('updateRequest error' + err);
+				res.json ({
+					statusCode: '500',
+	  			statusMsg: err,
+        	total: '0'
+		   	});
+		  } else if (!obj) {
+			  console.log('updateRequest error' + err);
+			  res.json ({
+				  statusCode: '500',
+				  statusMsg: err,
+      	  total: '0'
+			  });
+		  } else {
+			  res.json ({
+				  statusCode: '200',
+				  statusMsg: 'success',
+      	  total: '0'
+			  });
+		  }
+		});
+}
+
+requestRoomSchema.statics.deleteRequest = async function(req, res) {
+	console.log("deleteRequest : " + req.params.memberId);
+	var room = await this.findOneAndDelete(
+	  {'memberId': req.params.memberId},
+		function (err, obj) {
+			if (err) {
+				console.log('deleteRoom error' + err);
+				res.json ({
+					statusCode: '500',
+	  			statusMsg: err,
+        	total: '0'
+		   	});
+		  } else if (!obj) {
+			  console.log('deleteRoom error' + err);
+			  res.json ({
+				  statusCode: '500',
+				  statusMsg: err,
+      	  total: '0'
+			  });
+		  } else {
+			  res.json ({
+				  statusCode: '200',
+				  statusMsg: 'success',
+      	  total: '0'
+			  });
+			  console.log('delete obj : ' + obj);
+		  }
+  });
+}
+
+requestRoomSchema.statics.getMyRequestInfo = async function(req, res) {
+ 	// _id-room and roomID-request join
+	// memberID search in memberID-request .....
+	// res have it...
+}
+
+/*
+requestRoomSchema.statics.findMemberId = async function(req,res){
+	console.log('find... Member Id');
+	var member = await this.findOne({
+		'memberId':req.body.memberId
+	});
+	console.log(req.body.memberId);
+	var result;
+	if(!member){
+		console.log('member not found');
+	}else{
+		result = member._id;
+	}
+	return result;
+}
+
+requestRoomSchema.statics.joinMember = async function(req,res){
+  console.log('join... Member');
+
+  var room = await this.findOne({
+	  'roomId':req.body.roomId
+  });
+
+  console.log('room :'+ room);
+  var result;
+  if(!room){
+	  console.log('room not found');
+	  result = false;
+  } else {
+	  console.log('join successes');
+	  console.log('mememeberId' + req.body.memberId);
+	  room.memberId.push(req.body.memberId);
+	  console.log('room status : '+ room.memberId);
+	  result = await room.save();
+  }
+}*/
+
+module.exports = mongoose.model('requestRoom',requestRoomSchema);
--- routes/routers.js
+++ routes/routers.js
@@ -41,65 +41,58 @@
 		await RoomModel.getRoomSearch(req, res);
 	});
 
-	/* 내 방에 신청한 사람 보기 */
-  app.get('/room/applyRoom/:memberId', async function(req, res) { 
-		/* TODO */
-	  console.log("get /room/applyRoom show requested member:" + req.params.memberId);
-		res.json({
-			statusCode: '200',
-			statusMsg: 'success',
-			total: '2',
-			resultItems: [
-		    {
-				  memberId: 'dlgmlals3'
-			  },
-				{
-					memberId: 'gjrbdnjs'
-				}
-			]
-		});
+	/* todo */
+	// 2 query is only 1.....
+
+	/* request to room  CRUD */
+	app.post('/room/applyRoom', async function(req, res) { 
+	  console.log("post /room/applyRoom register apply room"); 
+	  console.log("RoomMaker Id : " + req.body.memberId + " reuqest ID : " + 
+			req.body.requestMemberId);
+		// find room id from room using memberID
+		var RoomModel = require('../database/schema/room');
+		var roomId = await RoomModel.getRoomId(req.body.memberId);
+		console.log("room id : " + roomId);
+		// insert requestMemberId, roomId, status = "request"
+		if (roomId) {
+		  var requestModel = require('../database/schema/requestRoom');
+		  requestModel.insertRequest(req, roomId, "request", res);
+		}
 	});
 
-	app.post('/room/applyRoom', async function(req, res) { 
-		/* TODO */
-	  console.log("post /room/applyRoom register apply room"); 
-	  console.log("RoomMaker Id : " + req.body.memberId + "reuqest ID : " + 
-			req.body.requestMemberId);
+  /* get request info to room */
+  app.get('/room/applyRoom/:memberId', async function(req, res) { 
+	  console.log("get /room/applyRoom show requested member:" + req.params.memberId);
+		// find room id from room using memberID
+		var RoomModel = require('../database/schema/room');
+		var roomId = await RoomModel.getRoomId(req.params.memberId);
+	  // get request	
+		if (roomId) {
+		  var requestModel = require('../database/schema/requestRoom');
+		  requestModel.getRequest(roomId, res);
+		}
 	});
+
+	app.put('/room/applyRoom/:memberId', async function(req, res) {
+		console.log('PUT /room/applyRoom/:memberId');
+		var requestModel = require('../database/schema/requestRoom');
+		await requestModel.updateRequest(req, res);
+	})
 
 	app.delete('/room/applyRoom', async function(req, res) { 
-		/* TODO */
 	  console.log("delete /room/applyRoom"); 
-	  console.log("RoomMaker Id : " + req.body.memberId + "reuqest ID : " + 
+	  console.log("RoomMaker Id : " + req.body.memberId + " reuqest ID : " + 
 			req.body.requestMemberId);
+		var requestModel = require('../database/schema/requestRoom');
+		await requestModel.deleteRequest(req, res);
 	});
 
-	/* 멤버는 자신이 신청한 방장의 멤버아이디를 가지고 있어야함 */
-	/* 내가 신청한 방보기 */
-	app.get('/room/applyRoom/:memberId', async function(req, res) { 
-	  console.log("appliedRoom myId : " + req.params.memberId);
+	/* 내가 신청한 방보기 only R */
+	app.get('/room/myApplyInfo/:memberId', async function(req, res) { 
+	  console.log("my apply Room myId : " + req.params.memberId);
+		console.log('GET /room get roomList');
 		var RoomModel = require('../database/schema/room');
-		await RoomModel.getRoomElement(req, res);
-
-		res.json({
-			statusCode: '200',
-			statusMsg: 'success',
-			total: '1',
-			resultItems: [{
-				memberId: res.memberId,
-				title: res.title,
-				ageMin: res.ageMin,
-				ageMax: res.ageMax,
-				regDate: res.regDate,
-				region: res.region,
-				gender: res.gender,
-				price: res.price,
-				openUrl: res.openUrl,
-				intro: res.intro,
-				maxMemberNum:res.maxMemberNum,
-				registDate: res.registDate
-			}]
-		});
+		await RoomModel.getRoomList(req, res);
 	});
 }
 
